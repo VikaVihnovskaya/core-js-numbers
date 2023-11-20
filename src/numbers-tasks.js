320874ce889368113d154f0393fc4dc9ee560625
Vihnovskaya Vika <vihnovskaya1932@gmail.com>
2023-11-20 17:30:56 +0300

    feat: solve tasks to convert value to number

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -207,8 +207,16 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (typeof n !== 'number' || n <= 1 || n % 1 !== 0) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(n); i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
@@ -226,8 +234,16 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  try {
+    const intValue = parseInt(value, 10);
+    if (Number.isNaN(intValue)) {
+      return def;
+    }
+    return intValue;
+  } catch (err) {
+    return def;
+  }
 }
 
 /**
